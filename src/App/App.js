98b3534efd2f101f9f82b845f3a2b78fe95a06f6
Laurent Bassin <laurent@bassin.info>
2018-07-27 21:22:46 +0200

    Move router tags to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
+import {HashRouter, Route, Switch} from "react-router-dom";
 import ListPage from "./Planets/List/ListPage";
 import ViewPage from "./Planets/View/ViewPage";
 import NewPage from "./Planets/New/NewPage";
 
 const App = () => (
-    <div className={'container'}>
-        <main>
-            <Switch>
-                <Route exact path="/" component={ListPage}/>
-                <Route path="/planet/:id" component={ViewPage}/>
-                <Route path="/new" component={NewPage}/>
-            </Switch>
-        </main>
-    </div>
+    <HashRouter>
+        <div className={'container'}>
+            <main>
+                <Switch>
+                    <Route exact path="/" component={ListPage}/>
+                    <Route path="/planet/:id" component={ViewPage}/>
+                    <Route path="/new" component={NewPage}/>
+                </Switch>
+            </main>
+        </div>
+    </HashRouter>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-import {HashRouter} from "react-router-dom";
 import './index.css';
 
-ReactDOM.render((
-    <HashRouter>
-        <App/>
-    </HashRouter>
-), document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
